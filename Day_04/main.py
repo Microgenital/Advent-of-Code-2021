6abd037c79c4bc815e5c9e995fce51c36c901adb
Max Schmidt <DarkBlade4k@gmail.com>
2021-12-13 00:56:35 +0100

    prepared Day 4 files

---
+++ Day_04/main.py
@@ -0,0 +1,15 @@
+#############################
+# www.adventofcode.com/2021 #
+#     Challenge: Day 4      #
+#############################
+
+def main_01():
+    pass
+
+
+def main_02():
+    pass
+
+
+if __name__ == "__main__":
+    main_01()
